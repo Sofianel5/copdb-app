2e1a58a56bdf99aa2f63fbcd66fc5697017a4599
ssaladd <n7.saad@gmail.com>
2020-09-02 18:53:49 -0400

    cat

--- copdb/lib/screens/ProfileScreen.dart
+++ copdb/lib/screens/ProfileScreen.dart
@@ -14,12 +14,126 @@ class _ProfileScreen extends State<ProfileScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(8, 11, 17, 1),
-      body: Center(
-        child: Text(
-          "pf",
-          style: TextStyle(fontSize: 26),
-        ),
-      ),
+      body: Column(
+        children: [
+          Container(height: 120,),
+          Container(
+            width: 152,
+            height: 152,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/cat.jpg'),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF54C6EB).withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 0), // changes position of shadow
+                ),
+              ],
+              borderRadius: BorderRadius.circular(100),
+              border: Border.all(color: Color(0xFF54C6EB), width: 1.5),
+            ),
+            alignment: Alignment.center,
+          ),
+          Container(height: 20,),
+          Container(
+            child: Text(
+              "hamood habibi",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Container(height: 10,),
+          Container(
+            child: Text(
+              "@hamood",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white70
+              ),
+            ),
+          ),
+          Container(height: 30,),
+          Container(
+            width: 200,
+            height: 1,
+            decoration: BoxDecoration(
+              color: Color(0xFF54C6EB),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF54C6EB).withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 0), // changes position of shadow
+                ),
+              ],
+            ),
+            alignment: Alignment.center,
+          ),
+          Container(height: 30,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Icon(Icons.person_add, size: 32,),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Add Friends", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    Text('add your friends on copDB', style: TextStyle(fontSize: 12, color: Colors.white70),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Icon(Icons.share, size: 32,),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Invite Friends", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    Text('invite your friends to copDB', style: TextStyle(fontSize: 12, color: Colors.white70),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(height: 30,),
+          Container(
+            width: 200,
+            height: 1,
+            decoration: BoxDecoration(
+              color: Color(0xFF54C6EB),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF54C6EB).withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 0), // changes position of shadow
+                ),
+              ],
+            ),
+            alignment: Alignment.center,
+          ),
+        ],
+      )
     );
   }
 }
