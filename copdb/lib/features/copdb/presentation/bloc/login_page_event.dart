d47f0e6d10cc386e1061d6e478c1e3def1490089
Sofiane <slarbi10@stuy.edu>
2020-09-14 19:12:13 -0400

    bruh

--- copdb/lib/features/copdb/presentation/bloc/login_page_event.dart
+++ copdb/lib/features/copdb/presentation/bloc/login_page_event.dart
@@ -6,12 +6,12 @@ class LoginEvent extends RootEvent {
 }
 
 class LoginAttemptEvent extends LoginEvent {
-  final String email;
+  final String username;
   final String password;
-  LoginAttemptEvent({this.email, this.password});
-  LoginParams get params => LoginParams(email: email, password: password);
+  LoginAttemptEvent({this.username, this.password});
+  LoginParams get params => LoginParams(username: username, password: password);
   @override
-  List<Object> get props => [email, password];
+  List<Object> get props => [username, password];
 }
 class RequestSignupEvent extends LoginEvent {
   @override
