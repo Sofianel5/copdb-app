068af1558fe86c7485f5dda13401498c9e91817f
ssaladd <n7.saad@gmail.com>
2020-09-29 17:30:53 -0400

    oooops

--- copdb/lib/features/copdb/presentation/widgets/SearchBar.dart
+++ copdb/lib/features/copdb/presentation/widgets/SearchBar.dart
@@ -12,40 +12,52 @@ class SearchBar extends StatefulWidget {
 class _SearchBar extends State<SearchBar> {
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            height: 40,
-            width: MediaQuery.of(context).size.width*0.65,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(
-                color: Colors.white,
-              ),
-              color: Colors.transparent,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 40,
+          width: MediaQuery.of(context).size.width*0.65,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+              color: Colors.white,
             ),
-            child: 
-                Container(
-                  width: MediaQuery.of(context).size.width*0.55,
-                  height: 40,
-                  child: TextField(
-                    focusNode: widget.focusNode,
-                    onSubmitted: (value) => widget.onSearch(value),
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.search, color: Colors.white,),
-                      border: InputBorder.none,
-                      hintText: widget.text,
-                      hintStyle: TextStyle(color: Colors.white, fontSize: 12),
+            color: Colors.transparent,
+          ),
+          child: 
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 10,),
+                Icon(Icons.search),
+                Container(width: 10,),
+                Expanded(child:
+                  Container(
+                    padding: EdgeInsets.only(bottom: 4),
+                    alignment: Alignment.topCenter,
+                    width: 100,
+                    height: 40,
+                    child: TextField(
+                      focusNode: widget.focusNode,
+                      onSubmitted: (value) => widget.onSearch(value),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        /* icon: Icon(Icons.search, color: Colors.white,), */
+                        border: InputBorder.none,
+                        hintText: widget.text,
+                        hintStyle: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
                     ),
                   ),
                 ),
-       
-            
+                Container(width: 10)
+,              ],
+            ),
           ),
           Expanded(
             child: Container(),
